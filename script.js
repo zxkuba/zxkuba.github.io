0488424f4d6e0bdb6bde07d045a1f15ba155686c
zxkuba <jakub.krzeszowiak@gmail.com>
2020-07-15 17:46:53 +0200

    v4

--- script.js
+++ script.js
@@ -108,12 +108,10 @@ $(document).ready(function() {
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parents('[data-task-id]');
     var taskId = parentEl.attr('data-task-id');
-    var requestUrl = apiRoot + 'deleteTask';
+    var requestUrl = apiRoot;
 
     $.ajax({
-      url: requestUrl + '/?' + $.param({
-        taskId: taskId
-      }),
+      url: requestUrl + taskId,
       method: 'DELETE',
       success: function() {
         parentEl.slideUp(400, function() { parentEl.remove(); });
