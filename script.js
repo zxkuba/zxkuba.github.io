59faeb17d27b69d5837ba159ebc7e9b41be00fc6
zxkuba <jakub.krzeszowiak@gmail.com>
2020-07-18 18:27:05 +0200

    new API_v2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://agile-caverns-05456.herokuapp.com/v1/task/';
+  const apiRoot = 'https://agile-caverns-05456.herokuapp.com/v1/';
   const trelloApiRoot = 'https://agile-caverns-05456.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
